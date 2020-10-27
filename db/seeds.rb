763789942c6d58f994ba687a3d7aebafaafe3195
fumiaki yanagisawa <wakoshi10@gmail.com>
2020-10-27 22:15:07 +0900

    Seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-# ユーザーのデータ
+# # ユーザーのデータ
 10.times do |n|
-  name  = "example-#{n+1}"
+  name  = "User-#{n+1}"
   email = "example-#{n+1}@example.com"
   password = "password"
   User.create!(name:  name,
@@ -10,20 +10,17 @@
 end
 
 # 質問のデータ
-10.times do |n|
-  title  = "Question-#{n+1}"
-  content = "Question-#{n+1}"
-  user_id = rand(1..10)
-  Question.create!(title:  title,
-               content: content,
-               user_id: user_id)
-end
+Question.create(title: 'Windowsエラー', content: 'Windowsエラー', user_id: 1)
+Question.create(title: 'Lunuxエラー', content: 'Lunuxエラー', user_id: 2)
+Question.create(title: 'Oracleエラー', content: 'Oracleエラー', user_id: 3)
+Question.create(title: 'スクリプトエラー', content: 'スクリプトエラー', user_id: 4)
+Question.create(title: 'コンパイルエラー', content: 'コンパイルエラー', user_id: 5)
 
 # 回答のデータ
 10.times do |n|
   content = "Answer-#{n+1}"
   user_id = rand(1..10)
-  question_id = rand(1..10)
+  question_id = rand(1..5)
   Answer.create!(content: content,
                user_id: user_id,
                question_id:  question_id)
@@ -32,7 +29,7 @@ end
 # ブックマークのデータ
 10.times do |n|
   user_id = rand(1..10)
-  question_id = rand(1..10)
+  question_id = rand(1..5)
   Bookmark.create!(user_id: user_id,
                 question_id:  question_id)
 end
